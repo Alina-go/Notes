640f19e65cfe55b1cdb864e114df4aabb8b257cb
Alina-go <goryainova.97@mail.ru>
2021-10-20 18:35:40 +0300

    Add lackey test

---
+++ GUITests/lackey_test.py
@@ -0,0 +1,18 @@
+import sys, os, subprocess
+from lackey import Key, click, wait, type, paste
+
+waitTime = 60
+
+process = subprocess.Popen("C:\\Notes\\Build\\Notes\\debug\\Notes.exe")
+
+# wait("wait_war.png",waitTime)
+# click("ok.png")
+
+# wait("open.png", waitTime)
+click("add_vb.png")
+# click("note_1.png")
+# click("rename_note_1.png")
+# type("a", Key.CTRL)
+# paste("My_note")
+# click("save_note.png")
+click("exit_vb.png")
